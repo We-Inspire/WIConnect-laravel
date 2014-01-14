3a4caed48b114ac0428d9543a959a726e3d1397d
Christoph Burger <office@christophburger.eu>
2014-01-14 09:21:47 +0100

    test

--- src/WeInspire/WeInspire/WeInspire.php
+++ src/WeInspire/WeInspire/WeInspire.php
@@ -13,7 +13,7 @@ class WeInspire implements HttpKernelInterface {
 
 	public function handle(SymfonyRequest $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true) {
 
-		\Illuminate\Foundation\Application::requestClass("WeInspire\Request\Request");
+		$request = new WeInspire\Request\Request::createFromGlobals();
 
 		//$this->app->requestClass("WeInspire\Request\Request");
 

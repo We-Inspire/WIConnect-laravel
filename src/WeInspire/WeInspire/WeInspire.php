17b7d56f7b28f092e48bc487c9207e073d7ba508
Christoph Burger <office@christophburger.eu>
2014-01-14 08:58:42 +0100

    added middleware file

---
+++ src/WeInspire/WeInspire/WeInspire.php
@@ -0,0 +1,20 @@
+<?php namespace WeInspire\WeInspire;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+class WeInspire extends HttpKernelInterface {
+
+	protected $app;
+
+	public function __construct(HttpKernelInterface $app) {
+		$this->app = $app;
+	}
+
+	public function handle(SymfonyRequest $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true) {
+		$response = $this->app->handle($request, $type, $catch);
+
+		die(var_dump($response));
+	}
+
+}

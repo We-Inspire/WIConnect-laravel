7804fa7d91d84ba7a1afdb18990fea2248d5f750
Christoph Burger <office@christophburger.eu>
2014-01-22 16:16:53 +0100

    Revert b65f112..8c07fcc
    This rolls back to commit b65f112b33eec07c9acb8e48be7c22b7002c2311.

--- src/WeInspire/WeInspire/Application.php
+++ src/WeInspire/WeInspire/Application.php
@@ -2,11 +2,15 @@
 
 class Application extends \Illuminate\Foundation\Application {
 
-	public function run($url = "", $key = "")
+	public function run($data = "", $key = "")
 	{
 		// [TODO] fake request
 
 		$privkey = "test";
+
+		$url = $data[0];
+
+		
 
 		//return var_dump($this['request']->path());
 		if(isset($url) && !empty($url) && !empty($key) && $privkey == $key) {
